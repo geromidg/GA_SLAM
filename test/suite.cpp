f0acbc7abe21745173458d3815f8b772329dce4e
Dimitris Geromichalos <geromidg@gmail.com>
2018-01-16 11:54:28 +0100

    Remove test directory

--- test/suite.cpp
+++
@@ -1,5 +0,0 @@
-// Do NOT add anything to this file
-// This header from boost takes ages to compile, so we make sure it is compiled
-// only once (here)
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
